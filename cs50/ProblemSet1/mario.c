d7076b7ee3722834c4974b4f3209e90d86bf043c
sergkol557 <sergkol557@gmail.com>
2017-02-13 15:25:17 +0200

    loop fixes

--- cs50/ProblemSet1/mario.c
+++ cs50/ProblemSet1/mario.c
@@ -16,16 +16,24 @@ int getPiramideHeight(){
 void drawPiramideRaw(int row, int height){
      int row_hashes=row+1;
      int row_spaces=height-row_hashes;
-     for(int count_spaces=0;count_spaces<row_spaces;count_spaces++)printf(" ");
-     for(int count_hashes=0;count_hashes<row_hashes;count_hashes++)printf("#");
+     for(int count_spaces=0;count_spaces<row_spaces;count_spaces++){
+         printf(" ");
+     }
+     for(int count_hashes=0;count_hashes<row_hashes;count_hashes++){
+         printf("#");
+     }
      printf("  ");
-     for(int count_hashes=0;count_hashes<row_hashes;count_hashes++)printf("#");
+     for(int count_hashes=0;count_hashes<row_hashes;count_hashes++){
+         printf("#");
+     }
      printf("\n");
 }
 //=================================================================================================================
 int main(void){
     int piramide_height=getPiramideHeight();
-    for(int count_row=0;count_row<piramide_height;count_row++)drawPiramideRaw(count_row,piramide_height);
+    for(int count_row=0;count_row<piramide_height;count_row++){
+        drawPiramideRaw(count_row,piramide_height);
+    }
     return 0;
 }
 //=================================================================================================================
